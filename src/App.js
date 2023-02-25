1589623a76d225b15243a2ce3c463c0599bed471
kvekeria <95439093+kvekeria@users.noreply.github.com>
2023-02-25 18:10:43 -0500

    Added Mission Statement

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import './image.css'
+import './writing.css'
 import NavBar from './navigationbar.js'
+import Mission from './mission.js'
+import myImage from './clear logo.png';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1> VoteMA </h1>
-      <NavBar />
+      <div className="image-container">
+        <img src={myImage} />
+      </div>
+      <Mission/>
+      <NavBar/>
     </div>
   );
 }
---
+++ src/mission.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './writing.css'
+function Mission() {
+    return (
+      <div className="text-container">
+        <h2>Our Mission</h2>
+        <p>Politicians exist to serve the needs of the people. Voting exists to ensure that politicians uphold their duty. Your vote is your political voice. It represents your values and needs, and it forces those in power to represent your values and needs. This is why WeVote exists. We are a community of volunteers who seek to improve political representation and accountability by increasing voter turnout in underrepresented communities. In order to correct structural, systemic, and institutional inequalities, we must have representatives who fight to represent all people, with a focus on historically marginalized groups: low-income communities, youth and senior citizens, communities of colors, and people with disabilities. We hope to facilitate the process of voting by consolidating voter resources to get people registered to vote and then to help individuals cast those votes. Finally, we seek to improve voter literacy and turnout so that voters are both inspired and empowered with the knowledge to elect those who best represent their interests. At WeVote, we recognize that elections are for everyone, and we seek to give power to the people. 
+        </p>
+      </div>
+    );
+  }
+  export default Mission;
